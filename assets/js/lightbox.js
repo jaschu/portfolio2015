3fd53fa5512404cb95f019ea467a40845f8ab307
Jason Schupp <jaschu+github@gmail.com>
2015-07-28 21:48:18 -0700

    slide transition CSS for entering, leaving, and slide changes

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -18,9 +18,9 @@ Let's see
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var SLIDES = [
-  { thumbnail: 'assets/images/screenshots/bsu@2x.jpg', caption: 'This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.' },
-  { thumbnail: 'assets/images/screenshots/alma@2x.jpg', caption: 'One more caption, one more screenshot.' },
-  { thumbnail: 'assets/images/screenshots/lewis-clark@2x.jpg', caption: 'Third slide! What, fantastic. That third slide worked.' }
+  { thumbnail: 'bsu@2x.jpg', caption: 'This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.' },
+  { thumbnail: 'alma@2x.jpg', caption: 'One more caption, one more screenshot.' },
+  { thumbnail: 'lewis-clark@2x.jpg', caption: 'Third slide! What, fantastic. That third slide worked.' }
 ];
 
 // var SlideControls = React.createClass({
@@ -37,12 +37,12 @@ var SLIDES = [
 var Slide = React.createClass({displayName: "Slide",
     render: function(){
       var displaySlide = this.props.slides[this.props.current]; // First slide for now
-      console.log(this.props.current);
+      //console.log(this.props.current);
       return (
         React.createElement("figure", null, 
           React.createElement("div", {className: "thumb-wrapper"}, 
             React.createElement("div", {className: "thumb-pane"}, 
-              React.createElement("img", {src: displaySlide.thumbnail, alt: ""})
+              React.createElement("img", {src: "assets/images/screenshots/" + displaySlide.thumbnail, alt: ""})
             )
           ), 
           React.createElement("figcaption", null, 
@@ -77,14 +77,16 @@ var Lightbox = React.createClass({displayName: "Lightbox",
       //    </div>
       //  );
       return (
-        React.createElement("div", {className: "slide"}, 
-          React.createElement(ReactCSSTransitionGroup, {transitionName: "slide"}, 
-            React.createElement(Slide, {slides: this.props.slides, current: this.state.currentSlide})
+        React.createElement(ReactCSSTransitionGroup, {transitionName: "slide", transitionAppear: true}, 
+        React.createElement("div", {className: "slide", key: "slidekey"}, 
+          React.createElement(ReactCSSTransitionGroup, {transitionName: "slideinfo"}, 
+            React.createElement(Slide, {slides: this.props.slides, current: this.state.currentSlide, key: this.props.slides[this.state.currentSlide].thumbnail})
           ), 
-          React.createElement("div", {className: "controls"}, 
+          React.createElement("div", {className: "controls", key: "controlskey"}, 
             React.createElement("a", {href: "#", onClick: this.prevSlide, className: "left"}, "Previous"), 
             React.createElement("a", {href: "#", onClick: this.nextSlide, className: "right"}, "Next")
           )
+        )
         )
       );
     }
--- assets/src/jsx/lightbox.jsx
+++ assets/src/jsx/lightbox.jsx
@@ -18,9 +18,9 @@ Let's see
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 
 var SLIDES = [
-  { thumbnail: 'assets/images/screenshots/bsu@2x.jpg', caption: 'This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.' },
-  { thumbnail: 'assets/images/screenshots/alma@2x.jpg', caption: 'One more caption, one more screenshot.' },
-  { thumbnail: 'assets/images/screenshots/lewis-clark@2x.jpg', caption: 'Third slide! What, fantastic. That third slide worked.' }
+  { thumbnail: 'bsu@2x.jpg', caption: 'This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption. This is a figure caption.' },
+  { thumbnail: 'alma@2x.jpg', caption: 'One more caption, one more screenshot.' },
+  { thumbnail: 'lewis-clark@2x.jpg', caption: 'Third slide! What, fantastic. That third slide worked.' }
 ];
 
 // var SlideControls = React.createClass({
@@ -37,12 +37,12 @@ var SLIDES = [
 var Slide = React.createClass({
     render: function(){
       var displaySlide = this.props.slides[this.props.current]; // First slide for now
-      console.log(this.props.current);
+      //console.log(this.props.current);
       return (
         <figure>
           <div className="thumb-wrapper">
             <div className="thumb-pane">
-              <img src={displaySlide.thumbnail} alt="" />
+              <img src={"assets/images/screenshots/" + displaySlide.thumbnail} alt="" />
             </div>
           </div>
           <figcaption>
@@ -77,15 +77,17 @@ var Lightbox = React.createClass({
       //    </div>
       //  );
       return (
-        <div className="slide">
-          <ReactCSSTransitionGroup transitionName="slide">
-            <Slide slides={this.props.slides} current={this.state.currentSlide} />
+        <ReactCSSTransitionGroup transitionName="slide" transitionAppear={true}>
+        <div className="slide" key="slidekey">
+          <ReactCSSTransitionGroup transitionName="slideinfo">
+            <Slide slides={this.props.slides} current={this.state.currentSlide} key={this.props.slides[this.state.currentSlide].thumbnail} />
           </ReactCSSTransitionGroup>
-          <div className="controls">
+          <div className="controls" key="controlskey">
             <a href="#" onClick={this.prevSlide} className="left">Previous</a>
             <a href="#" onClick={this.nextSlide} className="right">Next</a>
           </div>
         </div>
+        </ReactCSSTransitionGroup>
       );
     }
 });
